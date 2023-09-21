7eaf939de726042cb53e5a3d5d31a54b2088c5b9
Manikanthabaddireddy <Manibaddireddy7571@gmail.com>
2023-09-21 21:52:14 +0530

    linkedin added

--- assets/configs.py
+++ assets/configs.py
@@ -3,5 +3,5 @@ PAGE_ICON="📜"
 NAME="Manikantha Baddireddy:wave:"
 DESCRIPTION="Building complex pipelines with azure tech stack.Able to write expert level sql statements."
 POSITION="Data Enginner"
-SOCIAL_MEDIA={"LinkedIn": "www.linkedin.com/in/manikantha-baddireddy-52797b249","github": "https://github.com/Manikanthabaddireddy"}
+SOCIAL_MEDIA={"LinkedIn": "https://www.linkedin.com/in/manikantha-baddireddy-52797b249/","github": "https://github.com/Manikanthabaddireddy"}
 EMAIL="manibaddireddy7571@gmail.com"
