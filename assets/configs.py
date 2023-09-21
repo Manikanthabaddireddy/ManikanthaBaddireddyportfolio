6ce1e14a8f42c88159f98c9df97849fc899161be
Manikanthabaddireddy <Manibaddireddy7571@gmail.com>
2023-09-21 21:59:22 +0530

    adding description

--- assets/configs.py
+++ assets/configs.py
@@ -1,7 +1,7 @@
 PAGE_TITLE="DIGITAL CV |MANI"
 PAGE_ICON="📜"
 NAME="Manikantha Baddireddy:wave:"
-DESCRIPTION="Building complex pipelines using the Azure tech stack often involves integrating various Azure services to create data pipelines, automate processes, and manage data workflows. Additionally, having expertise in SQL is crucial for working with relational databases and manipulating data within these pipelines."
+DESCRIPTION="Building complex pipelines using the Azure tech stack often involves integrating various Azure services to create data pipelines, automate processes, and manage data workflows. Additionally, expert in SQL  for working with relational databases and analytical workloads."
 POSITION="Data Enginner"
 SOCIAL_MEDIA={"LinkedIn": "https://www.linkedin.com/in/manikantha-baddireddy-52797b249/","github": "https://github.com/Manikanthabaddireddy"}
 EMAIL="manibaddireddy7571@gmail.com"
