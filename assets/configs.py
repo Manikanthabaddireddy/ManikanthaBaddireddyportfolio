91bc498fd608c8075631e52acd020ef76e8d1ceb
Manikanthabaddireddy <Manibaddireddy7571@gmail.com>
2023-09-25 08:59:40 +0530

    changed github url

--- assets/configs.py
+++ assets/configs.py
@@ -5,4 +5,4 @@ DESCRIPTION="Building complex pipelines using the Azure tech stack often involve
 POSITION="Data Enginner"
 SOCIAL_MEDIA={"LinkedIn": "https://www.linkedin.com/in/manikantha-baddireddy-52797b249/","github": "https://github.com/Manikanthabaddireddy"}
 EMAIL="manibaddireddy7571@gmail.com"
-Python="https://github.com/Manikanthabaddireddy/webscraping/blob/main/"
+Python="https://github.com/Manikanthabaddireddy/quotescrapingauthorguessing/"
